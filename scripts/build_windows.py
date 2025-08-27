40274ad909c713af53a551ddc3479eaea7915c05
Keyyq YT <ytkeyyq@gmail.com>
2025-08-27 18:00:58 +0200

    logo

--- scripts/build_windows.py
+++ scripts/build_windows.py
@@ -1,13 +1,16 @@
 import subprocess
 import os
 
-os.system(
-    'cd "c:\\Blush\\src\\" && g++ blush.cpp -o ..\\build\\debug-x64\\blush'
-)
-
 def buildBlushWindows(src, args=None):
+    cpp_files = []
+    for root, dirs, files in os.walk(src):
+        for file in files:
+            if file.endswith(".cpp"):
+                cpp_files.append(os.path.join(root, file))
+
+    build_path = os.path.join(src, "..", "build", "debug-x64", "blush")
     subprocess.run(
-        ["g++", "blush.cpp", "-o", "..\\build\\debug-x64\\blush"],
+        ["g++", "-std=c++17"] + cpp_files + ["-o", build_path],
         cwd=src,
         check=True
     )
--- scripts/run_windows.py
+++ scripts/run_windows.py
@@ -3,8 +3,6 @@ import sys
 import os
 from build_windows import buildBlushWindows
 
-# Use --rebuild argument to rebuild it
-
 src_dir = "c:/Blush/src"
 build_path = "../build/debug-x64/blush.exe"
 build_dir = "../build/debug-x64"
@@ -12,14 +10,8 @@ build_dir = "../build/debug-x64"
 if not os.path.exists(build_dir):
     os.makedirs(build_dir)
 
-rebuild = False
-args_to_pass = []
-
-for arg in sys.argv[1:]:
-    if arg.lower() == "--rebuild":
-        rebuild = True
-    else:
-        args_to_pass.append(arg)
+rebuild = "--rebuild" in [arg.lower() for arg in sys.argv[1:]]
+args_to_pass = [arg for arg in sys.argv[1:] if arg.lower() != "--rebuild"]
 
 if rebuild or not os.path.exists(build_path):
     buildBlushWindows(src_dir)
