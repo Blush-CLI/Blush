6125e9e82d97036aa6b7b31c5f2b7fcdc2954a62
x4raynixx <ytkeyyq@gmail.com>
2025-09-12 18:22:39 +0200

    Changelog: History System Update;

--- scripts/build.py
+++ scripts/build.py
@@ -8,7 +8,7 @@ def buildBlush(src, args=None):
         if any(part in ["installer"] for part in root.split(os.sep)):
             continue
         for file in files:
-            if file.endswith((".cpp")):
+            if file.endswith(".cpp"):
                 cpp_files.append(os.path.join(root, file))
 
     build_dir = os.path.join(src, "..", "build", "debug-x64")
@@ -22,16 +22,21 @@ def buildBlush(src, args=None):
         from compile_icon import compile_icon
         res_file = compile_icon()
 
-    compile_cmd = ["g++", "-std=c++23"]
+    compile_cmd = ["g++", "-std=c++23", "-Wall", "-static-libgcc", "-static-libstdc++"]
     if system_platform == "Windows":
-        compile_cmd += ["-static-libgcc", "-static-libstdc++", "-static"]
+        compile_cmd += ["-static"]
 
     compile_cmd += cpp_files
     if res_file:
         compile_cmd.append(res_file)
-    compile_cmd += ["-o", build_path, "-lcurl"]
+
+    if system_platform != "Windows":
+        compile_cmd += ["-lcurl"]
+
+    compile_cmd += ["-o", build_path]
 
     subprocess.run(compile_cmd, check=True)
+
     if res_file:
         os.remove(res_file)
 
